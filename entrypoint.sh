6b153e1a653e93c4bf0eabaa7374cedc7ee56afb
dwdraju <rajudawadinp@gmail.com>
2020-08-26 19:21:01 +0545

    unset default region

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 unset AWS_SECRET_ACCESS_KEY
 unset AWS_ACCESS_KEY_ID
-unset AWS_REGION
+unset AWS_DEFAULT_REGION
 
 aws configure set aws_access_key_id "${K8S_AWS_KEY}"
 aws configure set aws_secret_access_key "${K8S_AWS_SECRET}"
